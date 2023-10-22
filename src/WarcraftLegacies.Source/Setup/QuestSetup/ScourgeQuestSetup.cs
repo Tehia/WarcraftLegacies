6261f51aa5795ed9c9b7396180561c87cff0e392
Zakary Boven <zakaryboven@gmail.com>
2023-10-22 22:19:58 +1300

    idk

--- src/WarcraftLegacies.Source/Setup/QuestSetup/ScourgeQuestSetup.cs
+++ src/WarcraftLegacies.Source/Setup/QuestSetup/ScourgeQuestSetup.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using MacroTools;
+using MacroTools.Setup;
 using WarcraftLegacies.Source.Mechanics.Scourge.Plague;
 using WarcraftLegacies.Source.Quests.Scourge;
 using WarcraftLegacies.Source.Setup.FactionSetup;
@@ -7,17 +8,34 @@ using WCSharp.Shared.Data;
 
 namespace WarcraftLegacies.Source.Setup.QuestSetup
 {
-  public static class ScourgeQuestSetup
+  public sealed class ScourgeQuestSetup : IExecutableService
   {
-    public static void Setup(PreplacedUnitSystem preplacedUnitSystem, ArtifactSetup artifactSetup, AllLegendSetup allLegendSetup)
+    private readonly PreplacedUnitSystem _preplacedUnitSystem;
+    private readonly AllLegendSetup _allLegendSetup;
+    private readonly ArtifactSetup _artifactSetup;
+    private readonly ScourgeSetup _scourgeSetup;
+    private readonly LordaeronSetup _lordaeronSetup;
+    private readonly LegionSetup _legionSetup;
+
+    public ScourgeQuestSetup(ServiceCollection services)
+    {
+      _preplacedUnitSystem = services.GetRequired<PreplacedUnitSystem>();
+      _allLegendSetup = services.GetRequired<AllLegendSetup>();
+      _artifactSetup = services.GetRequired<ArtifactSetup>();
+      _scourgeSetup = services.GetRequired<ScourgeSetup>();
+      _lordaeronSetup = services.GetRequired<LordaeronSetup>();
+      _legionSetup = services.GetRequired<LegionSetup>();
+    }
+    
+    public void Execute()
     {
       QuestSpiderWar questSpiderWar = new(Regions.Ice_Crown,
-        preplacedUnitSystem.GetUnit(Constants.UNIT_N074_QUEEN_NEZAR_AZRET));
-      QuestKelthuzadLich questKelthuzadLich = new(allLegendSetup.Quelthalas.Sunwell, allLegendSetup.Scourge.Kelthuzad);
-      QuestKelthuzadDies questKelthuzadDies = new(questKelthuzadLich, allLegendSetup.Scourge.Kelthuzad);
+        _preplacedUnitSystem.GetUnit(Constants.UNIT_N074_QUEEN_NEZAR_AZRET));
+      QuestKelthuzadLich questKelthuzadLich = new(_allLegendSetup.Quelthalas.Sunwell, _allLegendSetup.Scourge.Kelthuzad);
+      QuestKelthuzadDies questKelthuzadDies = new(questKelthuzadLich, _allLegendSetup.Scourge.Kelthuzad);
       QuestEnKilahUnlock questEnKilahUnlock = new(Regions.EnKilahUnlock);
-      QuestDrakUnlock questDrakUnlock = new(Regions.DrakUnlock, allLegendSetup.Scourge.Kelthuzad);
-      QuestCultoftheDamned questCultoftheDamned = new(allLegendSetup.Scourge.Rivendare);
+      QuestDrakUnlock questDrakUnlock = new(Regions.DrakUnlock, _allLegendSetup.Scourge.Kelthuzad);
+      QuestCultoftheDamned questCultoftheDamned = new(_allLegendSetup.Scourge.Rivendare);
 
       var plagueParameters = new PlagueParameters();
       plagueParameters.PlagueRects = new List<Rectangle>
@@ -47,31 +65,29 @@ namespace WarcraftLegacies.Source.Setup.QuestSetup
         new Point(9418, 5396)
       };
 
-      QuestPlague questPlague = new(plagueParameters, LordaeronSetup.Lordaeron, LegionSetup.Legion, Regions.DeathknellUnlock, Regions.StratholmeScourgeBase, Regions.CaerDarrow);
+      QuestPlague questPlague = new(plagueParameters, _lordaeronSetup.Lordaeron, _legionSetup.Legion,
+        Regions.DeathknellUnlock, Regions.StratholmeScourgeBase, Regions.CaerDarrow);
 
-      QuestSapphiron questSapphiron = new(preplacedUnitSystem.GetUnit(Constants.UNIT_UBDR_SAPPHIRON_CREEP), allLegendSetup.Scourge.Kelthuzad);
-      QuestDestroyStratholme questDestroyStratholme = new(allLegendSetup.Lordaeron.Stratholme, allLegendSetup.Lordaeron.Arthas);
+      QuestSapphiron questSapphiron = new(_preplacedUnitSystem.GetUnit(Constants.UNIT_UBDR_SAPPHIRON_CREEP),
+        _allLegendSetup.Scourge.Kelthuzad);
+      QuestDestroyStratholme questDestroyStratholme =
+        new(_allLegendSetup.Lordaeron.Stratholme, _allLegendSetup.Lordaeron.Arthas);
       QuestLichKingArthas questLichKingArthas =
-        new(preplacedUnitSystem.GetUnit(Constants.UNIT_H00O_UTGARDE_KEEP_SCOURGE_OTHER), artifactSetup.HelmOfDomination,
-          allLegendSetup.Scourge.Arthas, allLegendSetup.Scourge.TheFrozenThrone);
-
-      //Setup
-      if (ScourgeSetup.Scourge != null)
-      {
-        ScourgeSetup.Scourge.AddQuest(questSpiderWar);
-        ScourgeSetup.Scourge.StartingQuest = questSpiderWar;
-        ScourgeSetup.Scourge.AddQuest(questDrakUnlock);
-        ScourgeSetup.Scourge.AddQuest(questEnKilahUnlock);
-        ScourgeSetup.Scourge.AddQuest(questCultoftheDamned);
-        ScourgeSetup.Scourge.AddQuest(questPlague);
-        ScourgeSetup.Scourge.AddQuest(questSapphiron);
-        //Early duel
-        ScourgeSetup.Scourge.AddQuest(questDestroyStratholme);
-        ScourgeSetup.Scourge.AddQuest(questKelthuzadLich);
-        ScourgeSetup.Scourge.AddQuest(questKelthuzadDies);
-        //Misc
-        ScourgeSetup.Scourge.AddQuest(questLichKingArthas);
-      }
+        new(_preplacedUnitSystem.GetUnit(Constants.UNIT_H00O_UTGARDE_KEEP_SCOURGE_OTHER),
+          _artifactSetup.HelmOfDomination,
+          _allLegendSetup.Scourge.Arthas, _allLegendSetup.Scourge.TheFrozenThrone);
+      
+      _scourgeSetup.Scourge.AddQuest(questSpiderWar);
+      _scourgeSetup.Scourge.StartingQuest = questSpiderWar;
+      _scourgeSetup.Scourge.AddQuest(questDrakUnlock);
+      _scourgeSetup.Scourge.AddQuest(questEnKilahUnlock);
+      _scourgeSetup.Scourge.AddQuest(questCultoftheDamned);
+      _scourgeSetup.Scourge.AddQuest(questPlague);
+      _scourgeSetup.Scourge.AddQuest(questSapphiron);
+      _scourgeSetup.Scourge.AddQuest(questDestroyStratholme);
+      _scourgeSetup.Scourge.AddQuest(questKelthuzadLich);
+      _scourgeSetup.Scourge.AddQuest(questKelthuzadDies);
+      _scourgeSetup.Scourge.AddQuest(questLichKingArthas);
     }
   }
 }
